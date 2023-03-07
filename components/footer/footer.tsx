652736903f21e4b153939715228b352999f45b62
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-03-08 06:36:52 +0700

    Fix Footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+interface FooterProps {
+  links: { href: string; label: string }[];
+}
 
-export default function Footer() {
+const Footer: React.FC<FooterProps> = ({ links }) => {
   return (
     <footer className="bg-blue">
       <div className="container mx-auto py-10 md:text-base text-sm">
@@ -15,33 +18,18 @@ export default function Footer() {
             className="mx-auto w-20 mb-10"
           />
         </div>
-        <div className="flex justify-center">
-          <ul className="flex space-x-20">
-            <li>
+        <div className="flex justify-center items-center">
+          <div className="flex space-x-16">
+            {links.map(({ href, label }) => (
               <Link
-                href="/"
-                className='className="font-poppins text-white hover:border-b-2 hover:border-white hover:transition hover:ease-in-out hover:delay-150 hover:duration-700 font-semibold uppercase"'
+                key={href}
+                href={href}
+                className="font-poppins text-white font-bold uppercase"
               >
-                Home
+                {label}
               </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className='className="font-poppins text-white hover:border-b-2 hover:border-white hover:transition hover:ease-in-out hover:delay-150 hover:duration-700 font-semibold uppercase"'
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contact"
-                className='className="font-poppins text-white hover:border-b-2 hover:border-white hover:transition hover:ease-in-out hover:delay-150 hover:duration-700 font-semibold uppercase"'
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+            ))}
+          </div>
         </div>
         <p className="text-white text-center font-opensans pt-10">
           © 2023 JumpStart Inventory Software. All rights reserved.
@@ -49,4 +37,6 @@ export default function Footer() {
       </div>
     </footer>
   );
-}
+};
+
+export default Footer;
