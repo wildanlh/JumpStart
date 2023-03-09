da65e61d9c9e817f611495c5d7e75ae7bca4b960
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-03-09 09:41:25 +0700

    Add inventory table

---
+++ components/table/inventoryTable.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+export default function InventoryTable() {
+  return (
+    <div className="p-4 sm:ml-64 sm:mr-64">
+      <div className="flex items-center justify-between pt-5 pb-10">
+        <div>
+          <h1 className="text-dark text-3xl font-bold font-popins">
+            Suppliers
+          </h1>
+        </div>
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-5 h-5 text-gray-500 "
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="block p-2 pl-10 text-sm border border-blue placeholder-blue text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-dark">
+          <thead className="text-sm text-white uppercase bg-orange font-poppins">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-light border-b font-medium font-inter">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-dark whitespace-nowrap "
+              >
+                Apple MacBook Pro
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
