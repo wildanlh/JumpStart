2187e83be8d797b715688f35e0c098616dd904fd
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-03-09 18:53:14 +0700

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,31 +7,29 @@ export default function Home() {
     <>
       <PageHead pageTitle="Home | JumpStart" />
       <PageLayout>
-        <div className="bg-blue flex justify-center items-center h-5/6 py-20">
-          <div className="mx-auto container">
-            <div className="flex flex-col items-center lg:flex-row py-5">
-              <div className="lg:w-1/2 px-4 py-10 lg:p-20 text-center lg:text-left">
-                <h1 className="font-poppins font-bold text-white text-3xl">
-                  All-in-one inventory management software
-                </h1>
-                <p className="font-inter text-base text-white">
-                  The only platform you will ever need to help run your
-                  business:integrated apps, kept simple, and loved by millions
-                  of happy users.
-                </p>
-              </div>
-              <div className="">
-                <Image
-                  src="images/product-manager.svg"
-                  alt="image"
-                  width={500}
-                  height={500}
-                  className=" object-center"
-                />
-              </div>
+        <section className="bg-blue h-1/2 py-20">
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="font-poppins font-bold text-white text-4xl">
+                All-in-one inventory management software
+              </h1>
+              <p className="font-inter text-base text-white">
+                The only platform you will ever need to help run your business:
+                integrated apps, kept simple, and loved by millions of happy
+                users.
+              </p>
+            </div>
+            <div className="lg:mt-0 lg:col-span-5 lg:flex">
+              <Image
+                src="/images/product-manager.svg"
+                alt="image"
+                width={500}
+                height={500}
+                className="w-full"
+              />
             </div>
           </div>
-        </div>
+        </section>
 
         <div className="bg-cyan flex justify-center items-center py-20">
           <div className="mx-auto container">
@@ -97,56 +95,54 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="bg-white flex justify-center items-center py-20">
-          <div className="mx-auto container">
-            <h1 className="font-poppins font-bold text-blue text-3xl text-center">
-              Connect your Inventory Without The Chaos
-            </h1>
-            <div className="flex flex-col items-center lg:flex-row py-5">
-              <div>
-                <Image
-                  src="images/product-manager3.svg"
-                  alt="image"
-                  width={500}
-                  height={500}
-                  className="mx-auto"
-                />
-              </div>
-              <div className="lg:w-1/2 px-4 py-10 lg:p-20 text-center lg:text-left">
-                <h1 className="font-poppins font-bold text-blue text-3xl">
-                  Always In Control.
-                </h1>
-                <p className="font-inter text-base text-blue">
-                  Creating and managing inventory has never been easier. With
-                  powerful tools that integrate with popular POS and e-commerce
-                  systems.
-                </p>
-              </div>
+        <section className="bg-white h-1/2 py-20">
+          <h1 className="font-poppins font-bold text-blue text-3xl text-center">
+            Connect your Inventory Without The Chaos
+          </h1>
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="lg:mt-0 lg:col-span-5 lg:flex border-l-2">
+              <Image
+                src="images/product-manager3.svg"
+                alt="image"
+                width={500}
+                height={500}
+                className="w-full"
+              />
             </div>
-            <div className="flex flex-col items-center lg:flex-row py-5">
-              <div className="lg:w-1/2 px-4 py-10 lg:p-20 text-center lg:text-left">
-                <h1 className="font-poppins font-bold text-blue text-3xl">
-                  Save Time And Money.
-                </h1>
-                <p className="font-inter text-base text-blue">
-                  JumpStarts inventory tracking software helps you find what is
-                  selling and what is not, saving you time and money by ensuring
-                  that your inventory is not only accurate, but has been
-                  prioritized.
-                </p>
-              </div>
-              <div className="">
-                <Image
-                  src="images/digital-nomad.svg"
-                  alt="image"
-                  width={500}
-                  height={500}
-                  className="mx-auto"
-                />
-              </div>
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="font-poppins font-bold text-blue text-3xl">
+                Always In Control.
+              </h1>
+              <p className="font-inter text-base text-blue">
+                Creating and managing inventory has never been easier. With
+                powerful tools that integrate with popular POS and e-commerce
+                systems.
+              </p>
             </div>
           </div>
-        </div>
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="font-poppins font-bold text-blue text-3xl">
+                Save Time And Money.
+              </h1>
+              <p className="font-inter text-base text-blue">
+                JumpStarts inventory tracking software helps you find what is
+                selling and what is not, saving you time and money by ensuring
+                that your inventory is not only accurate, but has been
+                prioritized.
+              </p>
+            </div>
+            <div className="lg:mt-0 lg:col-span-5 lg:flex border-r-2">
+              <Image
+                src="images/digital-nomad.svg"
+                alt="image"
+                width={500}
+                height={500}
+                className="w-full"
+              />
+            </div>
+          </div>
+        </section>
 
         <div className="bg-cream flex justify-center items-center py-20">
           <div className="mx-auto container">
